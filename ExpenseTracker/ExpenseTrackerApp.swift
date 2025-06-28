76ed0aa06d10094c022d23bae027a21c33c5bf7a
Md Rabbi <mdfrabbi1975@gmail.com>
2025-06-27 21:20:18 -0400

    Initial Commit: Set up expense tracker project

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by Md Rabbi on 6/27/25.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
